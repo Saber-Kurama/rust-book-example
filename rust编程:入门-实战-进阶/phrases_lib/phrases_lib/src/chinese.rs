2313ed42b77578b5a4f89248bc4fde4349a72008
guochao <463561248@qq.com>
2021-12-22 22:28:31 +0800

    ✨ feat: init

---
+++ rust编程:入门-实战-进阶/phrases_lib/phrases_lib/src/chinese.rs
@@ -0,0 +1,9 @@
+/*
+ * @Author: saber
+ * @Date: 2021-12-22 22:24:30
+ * @LastEditTime: 2021-12-22 22:24:31
+ * @LastEditors: saber
+ * @Description: 
+ */
+mod greetings;
+mod farewells;
---
+++ rust编程:入门-实战-进阶/phrases_lib/phrases_lib/src/lib.rs
@@ -0,0 +1,10 @@
+/*
+ * @Author: saber
+ * @Date: 2021-12-22 22:06:27
+ * @LastEditTime: 2021-12-22 22:24:15
+ * @LastEditors: saber
+ * @Description: 
+ */
+mod chinese;
+mod english;
+
