43b126a017e0044451337f9caaaa7a8651794506
guochao <463561248@qq.com>
2021-12-23 11:40:40 +0800

    ✨ feat: 调整目录

--- rust编程:入门-实战-进阶/phrases_lib/phrases_lib/src/chinese.rs
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: saber
- * @Date: 2021-12-22 22:24:30
- * @LastEditTime: 2021-12-22 22:24:31
- * @LastEditors: saber
- * @Description: 
- */
-mod greetings;
-mod farewells;
---
+++ rust编程_入门_进阶_实战/phrases_lib/phrases_lib/src/main.rs
@@ -0,0 +1,10 @@
+/*
+ * @Author: saber
+ * @Date: 2021-12-23 11:21:27
+ * @LastEditTime: 2021-12-23 11:26:05
+ * @LastEditors: saber
+ * @Description: 
+ */
+fn main() {
+ println!("hello in chinese: {}", "ss") 
+}
