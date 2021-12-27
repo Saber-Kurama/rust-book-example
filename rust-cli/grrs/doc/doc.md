005516014c7a6b486db7fcaa713341ff00e9cc54
guochao <463561248@qq.com>
2021-12-27 22:03:50 +0800

    添加kt

--- rust-cli/grrs/doc/doc.md
+++ rust-cli/grrs/doc/doc.md
@@ -2,3 +2,6 @@
 
 [Rust每周一库: structopt](https://colobu.com/2019/09/29/rust-lib-per-week-structopt/)
 [structopt](https://docs.rs/structopt/latest/structopt/)
+[写一个小型的 Rust CLI 工具后的总结]()https://jishuin.proginn.com/p/763bfbd6d7fa
+
+[[译] 用 Rust 打造你的第一个命令行工具](https://juejin.cn/post/6844903821307723789#heading-5)
---
+++ rust-cli/kt/src/main.rs
@@ -0,0 +1,44 @@
+use clap::{Arg, App};
+use std::fs::File;
+use std::path::Path;
+use std::io::{Read, Write};
+use std::process;
+
+fn main() {
+    let matches = App::new("kt")
+      .version("0.1.0")
+      .author("xxx")
+      .about("A drop in cat replacement written in Rust")
+      .arg(Arg::with_name("FILE")
+            .help("File to print.")
+            .empty_values(false)
+        )
+      .get_matches();
+
+    if let Some(file) = matches.value_of("FILE") {
+        println!("value: {}", file);
+        if Path::new(&file).exists() {
+            match File::open(file) {
+                Ok(mut f) => {
+                    let mut data = String::new();
+                    f.read_to_string(&mut data).expect("不能读取该文件");
+                    let stdout = std::io::stdout();
+                    let mut handle = std::io::BufWriter::new(stdout);
+                    match writeln!(handle, "{}", data) {
+                        Ok(_res) => {}
+                        Err(err) => {
+                            eprintln!("[kt Error] Unable to display the file contents. {:?}", err);
+                            process::exit(1);
+                        }
+                    }
+
+                }
+                Err(err) => {}
+            }
+        }
+    }else{
+        eprintln!("[kt Error]");
+        process::exit(1);
+    }   
+}
+
