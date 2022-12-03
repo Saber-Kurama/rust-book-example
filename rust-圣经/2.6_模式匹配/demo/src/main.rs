e9d5cf3b00118c3a65a5e124cd009eac52e26e18
guochao <463561248@qq.com>
2022-12-04 00:32:59 +0800

    ✨ feat:  模式匹配

--- rust-圣经/2.6_模式匹配/demo/src/main.rs
+++ rust-圣经/2.6_模式匹配/demo/src/main.rs
@@ -115,13 +115,164 @@
 //     assert!(matches!(bar, Some(x) if x > 2));
 // }
 
-// 变量覆盖
+// // 变量覆盖
+
+// fn main() {
+//     let age = Some(30);
+//     println!("在匹配前，age是{:?}", age);
+//     if let Some(age) = age {
+//         println!("匹配出来的age是{}", age);
+//     }
+//     println!("在匹配后，age是{:?}", age);
+// }
+
+// // 解构 Option
+
+// fn main() {
+//     fn plus_one(x: Option<i32>) -> Option<i32> {
+//         match x {
+//             None => None,
+//             Some(i) => Some(i + 1),
+//         }
+//     }
+
+//     let five = Some(5);
+//     let six = plus_one(five);
+//     let none = plus_one(None);
+//     println!("{:?}", six);
+//     println!("{:?}", none);
+// }
+
+// // while let 条件循环
+
+// fn main() {
+//     // 动态数组
+//     let mut stack = Vec::new();
+
+//     // 添加元素
+//     stack.push(1);
+//     stack.push(2);
+//     stack.push(3);
+
+//     while let Some(top) = stack.pop() {
+//         print!("{}", top)
+//     }
+// }
+
+// // for 循环
+// fn main() {
+//     let v = vec!['a', 'b', 'c'];
+//     for (index, value) in v.iter().enumerate() {
+//         println!("{} is at index {}", value, index)
+//     }
+// }
+
+// // 单分支多模式
+
+// fn main() {
+//     let x = 1;
+//     match x {
+//         1 | 2 => println!("1和2"),
+//         3 => println!("3"),
+//         _ => println!("其他"),
+//     }
+// }
+
+// // 通过序列 `..=` 匹配值的范围
+// fn main() {
+//     let x = 5;
+//     match x {
+//         1..=5 => println!("1到5"),
+//         _ => println!("其他"),
+//     }
+// }
+
+// // 解耦结构体
+
+// fn main() {
+//     struct Point {
+//         x: i32,
+//         y: i32,
+//     }
+
+//     let p = Point { x: 10, y: 20 };
+//     let Point { x: a, y: b } = p;
+//     println!("a is {}, b is {}", a, b);
+
+//     match p {
+//         Point { x, y: 21 } => println!("x 值是 {}", x),
+//         Point { x: 10, y } => println!("y 值是 {}", y),
+//         Point { x, y } => println!("x 值是  {}, y 值是 {}", x, y),
+//     }
+// }
+
+// // 解构枚举
+
+// enum Message {
+//     Quit,
+//     Move { x: i32, y: i32 },
+//     Write(String),
+//     ChangeColor(i32, i32, i32),
+// }
+// fn main() {
+//     let msg = Message::ChangeColor(0, 160, 255);
+
+//     match msg {
+//         Message::Quit => {
+//             println!("quit")
+//         }
+//         Message::Move { x, y } => {
+//             println!("x is {}, y is {}", x, y)
+//         }
+//         Message::Write(text) => println!("text is {}", text),
+//         Message::ChangeColor(r, g, b) => println!("r is {}, g is {}, b is {}", r, g, b),
+//     }
+// }
+
+// // 解构嵌套的结构体和枚举
+
+// enum Color {
+//     Rgb(i32, i32, i32),
+//     Hsv(i32, i32, i32),
+// }
+
+// enum Message {
+//     Quit,
+//     Move { x: i32, y: i32 },
+//     Write(String),
+//     ChangeColor(Color),
+// }
+
+// fn main() {
+//     let msg = Message::ChangeColor(Color::Hsv(0, 160, 255));
+
+//     match msg {
+//         Message::ChangeColor(Color::Rgb(r, g, b)) => println!("r is {}, g is {}, b is {}", r, g, b),
+//         Message::ChangeColor(Color::Hsv(h, s, v)) => println!("h is {}, s is {}, v is {}", h, s, v),
+//         _ => println!("其他"),
+//     }
+// }
+
+// // 匹配守卫 额外条件
+
+// fn main() {
+//     let num = Some(4);
+//     match num {
+//         Some(x) if x < 5 => println!("{}小于5的", x),
+//         Some(x) => println!("{}", x),
+//         None => (),
+//     }
+// }
 
 fn main() {
-    let age = Some(30);
-    println!("在匹配前，age是{:?}", age);
-    if let Some(age) = age {
-        println!("匹配出来的age是{}", age);
+    let x = Some(10);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {}", n),
+        _ => println!("Default case, x = {:?}", x),
     }
-    println!("在匹配后，age是{:?}", age);
+
+    println!("at the end: x = {:?}, y = {}", x, y);
 }
