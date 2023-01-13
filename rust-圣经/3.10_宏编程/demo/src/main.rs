4fa0f8373486da0004e44197f6992f0969baf512
guochao <463561248@qq.com>
2023-01-13 22:11:03 +0800

    ✨ feat:  update

---
+++ rust-圣经/3.10_宏编程/demo/src/main.rs
@@ -0,0 +1,15 @@
+// #[macro_export]
+// macro_rules! vec_new {
+//     ($($x:expr), *) => {
+//         let mut temp_vec = Vec::<T>::new()
+//         $(
+//             temp_vec.push($x)
+//         )*
+//         temp_vec
+//     };
+// }
+
+fn main() {
+    // let a = vec_new!(1, 2, 3);
+    println!("Hello, world!");
+}
